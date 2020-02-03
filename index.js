e4c09fbb1e75d55c4cfdec4b55db409660eee9ef
Lamp <ledlamp89@gmail.com>
2020-02-02 20:20:41 -0800

    finalize
    delete commented code
    i think it's ready
    todo test

--- index.js
+++ index.js
@@ -3,22 +3,11 @@ var Discord = require("discord.js");
 var MailInterface = require("./mailinterface");
 
 var dc = new Discord.Client();
-dc.login(process.env.TOKEN).then(() => { console.log("Logged in to Discord") });
+dc.login(process.env.DISCORD_TOKEN).then(() => { console.log("Logged in to Discord") });
 
 var mint = new MailInterface({
 	mbox: process.env.MBOX,
 	nodemailer: {
-		/*options: {
-			host: process.env.SMTP_HOST || "localhost",
-			port: process.env.SMTP_PORT || 25,
-			name: process.env.HOSTNAME,
-			from: process.env.MAIL_FROM,
-			auth: {
-				user: process.env.SMTP_USERNAME,
-				pass: process.env.SMTP_PASSWORD
-			}
-			logger: true;
-		},*/
 		options: process.env.SMTP_URI,
 		defaults: {
 			from: process.env.MAIL_FROM,
@@ -27,11 +16,9 @@ var mint = new MailInterface({
 	},
 });
 
-
 // discord to mms
 dc.on("message", async function(message){
     if (message.channel.id == process.env.DISCORD_CHANNEL_ID) {
-        //await mint.send(`${message.author.tag}: ${message.cleanContent}`); //TODO attachment support
 		try {
 			await mint.send({
 				subject: message.author.tag,
@@ -53,7 +40,7 @@ mint.on("mail", async function(mail){
 		if (mail.from.text != process.env.TARGET_EMAIL) {
 			require('fs').appendFileSync("unknown_emails.txt", JSON.stringify(mail, null, 4) + '\n\n\n\n');
 			return;
-		};
+		}
 		var textAttachment = mail.attachments.find(a => a.filename == "text_0.txt");
 		var msg = mail.text || textAttachment.content || mail.html || "<missing message>";
 		if (mail.subject) msg = `**${mail.subject}**\n${msg}`;
--- mailinterface.js
+++ mailinterface.js
@@ -6,14 +6,12 @@ var nodemailer = require("nodemailer");
 
 class MailInterface extends EventEmitter {
 	
-	constructor(opts) {
-		super();
-		Object.assign(this, opts);
-		this.mbox = this.mbox || `/var/mail/${require('os').getUser().username}`;
+	constructor(options) { super();
+		this.mbox = options.mbox || `/var/mail/${require('os').getUser().username}`;
 		fs.watch(this.mbox, eventType => {
 			if (eventType == "change") this.check();
 		});
-		this.transporter = nodemailer.createTransport(this.nodemailer.options, this.nodemailer.defaults);
+		this.transporter = nodemailer.createTransport(options.nodemailer.options, options.nodemailer.defaults);
 		this.transporter.verify((error, success) => {
 			error ? console.error(error) : console.log("SMTP connection verified", success);
 		});
@@ -24,7 +22,6 @@ class MailInterface extends EventEmitter {
 		console.log("checking mail");
 		if (fs.statSync(this.mbox).size == 0) return console.log("no mail");
 		var mbox = new Mbox(this.mbox);
-		var gotMail = false;
 		mbox.on("message", msg => {
 			simpleParser(msg).then(parsedMail => {
 				console.log("parsed mail:\n", parsedMail);
@@ -37,33 +34,6 @@ class MailInterface extends EventEmitter {
 			fs.writeFileSync(this.mbox, '');
 		});
 	}
-	
-	//async send(body) {
-		// var smtpClient = new SMTPClient(this.smtp);
-		// await smtpClient.connect();
-		// await smtpClient.greet({hostname: this.smtp.hostname});
-		// await smtpClient.mail({from: this.smtp.from});
-		// await smtpClient.rcpt({to: this.target});
-		// var msg = '\r\n' + body.replace(/\n/g, '\r\n') + '\r\n';
-		// /*var msg = 'MIME-Version: 1.0\r\n' +
-			// 'Content-Type: multipart/alternative; boundary="asdfhuiadfghviuarevhilu"\r\n' +
-			// "\r\n" +
-			// "--asdfhuiadfghviuarevhilu\r\n" +
-			// 'Content-Type: text/plain; charset="UTF-8"\r\n' +
-			// '\r\n' + body.replace(/\n/g, '\r\n') + '\r\n' +
-			// '\r\n' +
-			// "--asdfhuiadfghviuarevhilu\r\n" +
-			// 'Content-Type: text/html; charset="UTF-8"\r\n' +
-			// '\r\n<div dir="ltr">' + body.replace(/\n/g, '\r\n') + '</div>\r\n' +
-			// '\r\n' +
-			// '--asdfhuiadfghviuarevhilu--\r\n';*/
-		// await smtpClient.data(msg);
-		// await smtpClient.quit();
-		
-		//await transporter.sendMail({text});//WIP
-		// how bout just use the method directly
-		
-	//}
 	
 }
 
